afc2c828d8761e606949dcd948407de720ac91c5
ruchiraw <ruchira.wageesha@gmail.com>
2014-08-16 02:57:19 +0530

    updating

--- index.js
+++ index.js
@@ -8,11 +8,8 @@ var server = new proxy.RoutingProxy();
  */
 module.exports = function (allow) {
     return function (req, res, next) {
-        var xhost = req.header('x-host');
-        if (xhost) {
-            xhost = xhost.split(':');
-            var host = xhost[0];
-            var port = xhost.length === 2 ? xhost[1] : 80;
+        var host = req.header('x-host');
+        if (host) {
             var drones = allow[host];
             if (!drones || !drones.length) {
                 res.send(404, 'Not Found');
